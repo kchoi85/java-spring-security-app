781801f0bdf3c04885127c76dc615652574a3e5b
David Choi <davidchoi0304@gmail.com>
2022-08-07 16:36:56 -0400

    access and refresh token

--- src/main/java/io/kchoi85/userservice/filter/CustomAuthenticationFilter.java
+++ src/main/java/io/kchoi85/userservice/filter/CustomAuthenticationFilter.java
@@ -1,13 +1,18 @@
 package io.kchoi85.userservice.filter;
 
+import java.util.Date;
+import java.util.stream.Collectors;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 
 import io.kchoi85.userservice.model.User;
@@ -39,6 +44,23 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         User user = (User) authResult.getPrincipal();
 
         // JWT token algorithm
-        Algorithm algorithm = Algorithm.HMAC256("secret");
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+        String access_token = JWT
+                .create()
+                .withSubject(user.getUsername())
+                .withClaim("userId", user.getId())
+                // .withClaim("roles",
+                // user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000)) // 10 minutes
+                .withIssuer(request.getRequestURL().toString())
+                .sign(algorithm);
+
+        String refresh_token = JWT
+                .create()
+                .withSubject(user.getUsername())
+                .withClaim("userId", user.getId())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000)) // 30 minutes
+                .withIssuer(request.getRequestURL().toString())
+                .sign(algorithm);
     }
 }
