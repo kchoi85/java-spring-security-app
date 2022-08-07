41f0fa030c6352c67bc4717dafc4bd3c8589b77d
David Choi <davidchoi0304@gmail.com>
2022-08-07 15:10:12 -0400

    user controller + mysql properties

---
+++ src/main/java/io/kchoi85/userservice/controller/UserController.java
@@ -0,0 +1,25 @@
+package io.kchoi85.userservice.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.kchoi85.userservice.model.User;
+import io.kchoi85.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok().body((userService.getUsers()));
+    }
+}
