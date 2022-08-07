25f74c4c6bddbe96c6ce41c3db97133400eb22ee
David Choi <davidchoi0304@gmail.com>
2022-08-07 15:45:26 -0400

    user controller tweaks

--- src/main/java/io/kchoi85/userservice/UserserviceApplication.java
+++ src/main/java/io/kchoi85/userservice/UserserviceApplication.java
@@ -1,13 +1,35 @@
 package io.kchoi85.userservice;
 
+import java.util.ArrayList;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.kchoi85.userservice.model.Role;
+import io.kchoi85.userservice.model.User;
+import io.kchoi85.userservice.service.UserService;
 
 @SpringBootApplication
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_USER"));
+
+			userService.saveUser(new User(null, "Kihoon", "kchoi85", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Ree", "daniel102", "5678", new ArrayList<>()));
+
+			userService.addRoleToUser("kchoi85", "ROLE_ADMIN");
+		};
 	}
 
 }
--- src/main/java/io/kchoi85/userservice/controller/UserController.java
+++ src/main/java/io/kchoi85/userservice/controller/UserController.java
@@ -1,15 +1,20 @@
 package io.kchoi85.userservice.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import io.kchoi85.userservice.model.Role;
 import io.kchoi85.userservice.model.User;
 import io.kchoi85.userservice.service.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -22,4 +27,39 @@ public class UserController {
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok().body((userService.getUsers()));
     }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        URI uri = URI
+                .create(
+                        ServletUriComponentsBuilder
+                                .fromCurrentContextPath()
+                                .path("/api/v1/user/save")
+                                .toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI
+                .create(
+                        ServletUriComponentsBuilder
+                                .fromCurrentContextPath()
+                                .path("/api/v1/role/save")
+                                .toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form) {
+        userService.addRoleToUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+}
+
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
 }
