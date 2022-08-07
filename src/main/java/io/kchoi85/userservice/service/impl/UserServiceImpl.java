ab65be0d4bf046c23f950cab23d479e6d15fbf2a
David Choi <davidchoi0304@gmail.com>
2022-08-07 14:58:58 -0400

    add log for userserviceimpl

--- src/main/java/io/kchoi85/userservice/service/impl/UserServiceImpl.java
+++ src/main/java/io/kchoi85/userservice/service/impl/UserServiceImpl.java
@@ -23,16 +23,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User saveUser(User user) {
+        log.info("Saving new user to the database: {}", user);
         return userRepository.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+        log.info("Saving new role to the database: {}", role);
         return roleRepository.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to user {}", roleName, username);
         User user = userRepository.findByUsername(username);
         Role role = roleRepository.findByName(roleName);
         user.getRoles().add(role);
@@ -40,11 +43,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String username) {
+        log.info("Getting user with username: {}", username);
         return userRepository.findByUsername(username);
     }
 
     @Override
     public List<User> getUsers() {
+        log.info("Getting all users");
         return userRepository.findAll();
     }
 
